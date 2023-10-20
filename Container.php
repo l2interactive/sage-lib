d1bbfd71cd9e36d63b59b49f2ee4e5ff2ae7e4b4
asimonellil2 <33500900+asimonellil2@users.noreply.github.com>
2023-10-20 13:34:33 -0500

    Update Container.php
    Add expected terminating method expected by Application interface for Roots/Sage/Container

--- Container.php
+++ Container.php
@@ -6,4 +6,12 @@ use Illuminate\Container\Container as BaseContainer;
 
 class Container extends BaseContainer
 {
+    /**
+     * @param $callback
+     * @return void
+     */
+    public function terminating($callback)
+    {
+        $callback();
+    }
 }
